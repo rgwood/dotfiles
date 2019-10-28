7d67c064fb90d2f67d7939b04cb0702dabfff990
Reilly <reilly.wood@icloud.com>
2019-10-28 16:06:33 -0700

    Install fish

--- pre-install.sh
+++ pre-install.sh
@@ -14,21 +14,23 @@ else
 	echo "not applicable"
 fi
 
-# Check if zsh is installed
-echo -n "Checking for zsh... "
+# Check if fish is installed
+echo -n "Checking for fish... "
 if [[ "$OSTYPE" == "darwin"* ]]; then
-	if ! brew list zsh > /dev/null 2>&1; then
-		brew install zsh > /dev/null
+	if ! brew list fish > /dev/null 2>&1; then
+		brew install fish > /dev/null
 		echo "installed"
 	else
 		echo "already installed"
 	fi
 else
-	if ! [ -x "$(command -v zsh)" ]; then
+	if ! [ -x "$(command -v fish)" ]; then
 		if [ -x "$(command -v yum)" ]; then
-			sudo yum install -y zsh
+			sudo yum install -y fish
 		elif [ -x "$(command -v apt-get)" ]; then
-			sudo apt-get install -y zsh
+			sudo apt-add-repository ppa:fish-shell/release-3
+			sudo apt-get update
+			sudo apt-get install fish
 		else
 			echo "unknown package manager!"
 			exit 1
@@ -37,15 +39,4 @@ else
 	else
 		echo "already installed"
 	fi	
-fi
-
-# Check if oh-my-zsh is installed
-echo -n "Checking for oh-my-zsh... "
-OMZ_DIR="$HOME/.oh-my-zsh"
-if [ ! -d "$OMZ_DIR" ]; then
-  /bin/sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" > /dev/null
-  echo "installed"
-else
-  /bin/sh ~/.oh-my-zsh/tools/upgrade.sh > /dev/null 2>&1
-  echo "updated"
 fi
