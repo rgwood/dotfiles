9f11180c4dc9e73ffdcd356369e588c337407694
Reilly <reilly.wood@icloud.com>
2019-10-29 10:17:03 -0700

    Add jenv and editor config

--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,14 @@
 set PATH /Users/reillywood/.npm-global/bin $PATH
 set PATH $HOME/.cargo/bin $PATH
+set PATH $HOME/.jenv/bin $PATH
+status --is-interactive; and source (jenv init -|psub)
+bind \cx\ce edit_command_buffer
+
+switch (uname)
+case Linux
+    echo Hi Tux! This is config.fish speaking
+case Darwin
+    # Make VS Code the default editor
+    set -Ux EDITOR code --wait --new-window
+    set -Ux VISUAL code --wait --new-window
+end
