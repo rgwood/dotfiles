01455bb998f9e70b8fedf940a8844232305d8c6b
Reilly <reilly.wood@icloud.com>
2019-11-01 18:09:43 -0700

    Make jenv macOS-only

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,6 @@
 set PATH /Users/reillywood/.npm-global/bin $PATH
 set PATH $HOME/.cargo/bin $PATH
-set PATH $HOME/.jenv/bin $PATH
-status --is-interactive; and source (jenv init -|psub)
+
 # Bash binding for editing current buffer
 bind \cx\ce edit_command_buffer
 
@@ -10,9 +9,11 @@ set -Ux EDITOR code --wait --new-window
 set -Ux VISUAL code --wait --new-window
 
 # OS-specific stuff
-# switch (uname)
-# case Linux
-#     echo Hi Tux! This is config.fish speaking
-# case Darwin
+switch (uname)
+    # case Linux
+    #     echo Hi Tux! This is config.fish speaking
+    case Darwin
 #     echo Hi macOS! This is config.fish speaking
-# end
+        set PATH $HOME/.jenv/bin $PATH
+        status --is-interactive; and source (jenv init -|psub)
+end
