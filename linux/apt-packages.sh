4400b425ab65622fcd2e4ea5ad7b6f29f130e179
Reilly <reilly.wood@icloud.com>
2020-02-27 15:38:52 -0800

    apt packages

---
+++ linux/apt-packages.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Install packages that are commonly available via APT in the distros I use
+
+sudo apt install fish mosh iotop fzf bfs
+
+# needed for some Cargo packages
+sudo apt install llvm libclang-dev
