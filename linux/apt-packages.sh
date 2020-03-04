26a438a42317e2eb9166c04dace8f7aec2ace8ff
Reilly <reilly.wood@icloud.com>
2020-03-03 19:58:20 -0800

    add build-essential

--- linux/apt-packages.sh
+++ linux/apt-packages.sh
@@ -3,5 +3,8 @@
 
 sudo apt install fish mosh iotop fzf bfs python3-pip
 
+# gcc etc, needed for cargo
+sudo apt install build-essential
+
 # needed for some Cargo packages
 sudo apt install llvm libclang-dev
