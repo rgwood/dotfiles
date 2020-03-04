5a73d06928b2c7413376c07c9ce34dadf7017936
Reilly <reilly.wood@icloud.com>
2020-03-03 19:44:42 -0800

    add python

--- linux/apt-packages.sh
+++ linux/apt-packages.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Install packages that are commonly available via APT in the distros I use
 
-sudo apt install fish mosh iotop fzf bfs
+sudo apt install fish mosh iotop fzf bfs python3-pip
 
 # needed for some Cargo packages
 sudo apt install llvm libclang-dev
