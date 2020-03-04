c6d9743c77e50e608b25847ec3b7396afa8029e6
Reilly <reilly.wood@icloud.com>
2020-03-03 20:31:27 -0800

    move fzf to separate line

--- linux/apt-packages.sh
+++ linux/apt-packages.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 # Install packages that are commonly available via APT in the distros I use
 
-sudo apt install fish mosh iotop fzf bfs python3-pip
+sudo apt install fish mosh iotop python3-pip
+
+# not always available...
+sudo apt install fzf
 
 # gcc etc, needed for cargo
 sudo apt install build-essential
 
 # needed for some Cargo packages
-sudo apt install llvm libclang-dev
+sudo apt install llvm libclang-dev libssl-dev pkg-config
