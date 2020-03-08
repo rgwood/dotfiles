69a38bd0d0249b8d8b0db460a696cada463df7b0
Reilly Wood <reilly.wood@icloud.com>
2020-03-08 12:59:52 -0700

    Windows PS profile

---
+++ windows/powershellProfile.ps1
@@ -0,0 +1,4 @@
+# Symlink setup:
+# New-Item -ItemType symboliclink -Path "$HOME\OneDrive\Documents\PowerShell\profile.ps1" -Value "$HOME\dotfiles\windows\powershellProfile.ps1" -Force
+
+Set-Alias -Name which -Value where.exe
