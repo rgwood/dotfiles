da32dcaba66b837ac07cc349d7cc6dc264df4e38
Reilly Wood <reilly.wood@icloud.com>
2020-03-09 23:21:24 -0700

    Add touch alias, use fzf

---
+++ windows/installModules.ps1
@@ -0,0 +1,2 @@
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+Install-Module -Name PSFzf -AllowPrerelease 
--- windows/powershellProfile.ps1
+++ windows/powershellProfile.ps1
@@ -1,4 +1,7 @@
-# Symlink setup:
-# New-Item -ItemType symboliclink -Path "$HOME\OneDrive\Documents\PowerShell\profile.ps1" -Value "$HOME\dotfiles\windows\powershellProfile.ps1" -Force
-
 Set-Alias -Name which -Value where.exe
+
+Set-Alias -Name touch -Value Touch-File
+Function Touch-File ($fileName) { New-Item -ItemType file $fileName }
+
+
+Import-Module PSFzf -ArgumentList 'Ctrl+t','Ctrl+r'
