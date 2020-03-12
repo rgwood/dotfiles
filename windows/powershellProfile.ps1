f2420a66e384ba415db92705c19114b0257ce37d
Reilly Wood <reilly.wood@icloud.com>
2020-03-12 12:43:11 -0700

    Get-Help aliases

--- windows/powershellProfile.ps1
+++ windows/powershellProfile.ps1
@@ -1,7 +1,21 @@
 Set-Alias -Name which -Value where.exe
 
-Set-Alias -Name touch -Value Touch-File
 Function Touch-File ($fileName) { New-Item -ItemType file $fileName }
-
+Set-Alias -Name touch -Value Touch-File
 
 Import-Module PSFzf -ArgumentList 'Ctrl+t','Ctrl+r'
+
+function Get-HelpCustom ($command) {
+    Get-Help $command | less
+}
+Set-Alias gh Get-HelpCustom
+
+function Get-Examples ($command) {
+    Get-Help $command -Examples | less
+}
+Set-Alias ghe Get-Examples
+
+function Get-DetailedHelp ($command) {
+    Get-Help $command -Detailed | less
+}
+Set-Alias ghd Get-DetailedHelp
