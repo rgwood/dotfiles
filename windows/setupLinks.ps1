bc884d273115ec8d90a5a6c0c90011efda49aa59
Reilly Wood <reilly.wood@icloud.com>
2020-03-12 11:25:26 -0700

    Rename now that we have hardlinks

---
+++ windows/setupLinks.ps1
@@ -0,0 +1,10 @@
+# Symlink setup:
+
+New-Item -ItemType symboliclink -Path "$HOME\OneDrive\Documents\PowerShell\profile.ps1" -Value "$HOME\dotfiles\windows\powershellProfile.ps1" -Force
+
+# Is there any difference between $HOME and $Env:Userprofile?
+New-Item -ItemType symboliclink -Path "$Env:Userprofile\.npmrc" -Value "$Env:Userprofile\dotfiles\Windows\npmrc"
+
+# Hardlink b/c Windows Terminal settings don't auto-reload with symlinks
+# https://github.com/microsoft/terminal/issues/1812
+New-Item -ItemType HardLink -Path "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\profiles.json"  -Value "$HOME\dotfiles\windows\WindowsTerminalProfiles.json" -Confirm -Force
--- windows/symlinks.ps1
+++
@@ -1,6 +0,0 @@
-# Symlink setup:
-
-New-Item -ItemType symboliclink -Path "$HOME\OneDrive\Documents\PowerShell\profile.ps1" -Value "$HOME\dotfiles\windows\powershellProfile.ps1" -Force
-
-# Is there any difference between $HOME and $Env:Userprofile?
-New-Item -ItemType symboliclink -Path "$Env:Userprofile\.npmrc" -Value "$Env:Userprofile\dotfiles\Windows\npmrc"
