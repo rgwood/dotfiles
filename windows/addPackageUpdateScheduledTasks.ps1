f7c4e9990447c9883df008385e092bc718af3653
Reilly Wood <reilly.wood@canalyst.com>
2021-01-15 14:23:13 -0800

    Add script to create auto-update tasks for Cargo and Scoop

---
+++ windows/addPackageUpdateScheduledTasks.ps1
@@ -0,0 +1,21 @@
+
+function TaskExists ($name) { Get-ScheduledTask | Where-Object { $_.TaskName -like $name }}
+
+$cargoTaskName = "Update Cargo Packages"
+If(TaskExists($cargoTaskName)) {
+    echo "Task '${cargoTaskName}' already exists, not creating"
+} else {
+    $action = New-ScheduledTaskAction -Execute (which cargo) -Argument 'install-update --all'
+    $trigger = New-ScheduledTaskTrigger -Daily -At 4am -RandomDelay (New-TimeSpan -Hours 1)
+    Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $cargoTaskName
+}
+
+$scoopTaskName = "Update Scoop Packages"
+If(TaskExists($scoopTaskName)) {
+    echo "Task '${scoopTaskName}' already exists, not creating"
+} else {
+    $path = which scoop | where { $_.endswith("cmd")}
+    $action = New-ScheduledTaskAction -Execute $path -Argument 'update *'
+    $trigger = New-ScheduledTaskTrigger -Daily -At 4am -RandomDelay (New-TimeSpan -Hours 1)
+    Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $scoopTaskName
+}
