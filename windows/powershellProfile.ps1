de80a0b0fa6b486f327b0f59f056fd441f61e9ff
Reilly Wood <reilly.wood@icloud.com>
2021-10-02 11:09:06 -0700

    Opt back in to .NET telemetry. I wanna make .NET better.

--- windows/powershellProfile.ps1
+++ windows/powershellProfile.ps1
@@ -27,4 +27,4 @@ Invoke-Expression (&starship init powershell)
 $scriptDir = (Resolve-Path "~/dotfiles/windows")
 New-Alias -Name wat -Value $scriptDir\wat.ps1
 
-$env:DOTNET_CLI_TELEMETRY_OPTOUT = 1
+# $env:DOTNET_CLI_TELEMETRY_OPTOUT = 1
