66d5107873dbc47faed0cc615b89bac3656b33f1
Reilly Wood <reilly.wood@icloud.com>
2021-12-13 21:43:34 -0800

    Add Debian setup script

---
+++ linux/debian-11.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# essentials
+sudo apt install git zsh rsync micro fzf
+
+# nice to haves
+sudo apt install mosh iotop python3-pip -y
+
+# needed for some Cargo packages
+sudo apt install build-essential llvm libclang-dev libssl-dev pkg-config -y
