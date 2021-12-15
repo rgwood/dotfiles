b4ac4fc532fa80c45e06eb0dbcacd1016f78be14
Reilly Wood <reilly.wood@canalyst.com>
2021-12-14 22:16:58 -0800

    Add Homebrew scripts for Linux

---
+++ brew/install.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# TODO remove unnecessary double bash
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
---
+++ brew/packages.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Some tools that I can't find in other binary package managers on Linux
+brew install lazygit nushell ripgrep
