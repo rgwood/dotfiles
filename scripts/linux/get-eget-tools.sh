4a47811668c59662c82ac12968a5bf6448454516
Reilly Wood <reilly.wood@icloud.com>
2021-12-26 22:49:45 -0800

    Add script to download Rust tools via eget

---
+++ scripts/linux/get-eget-tools.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+pushd ~/bin
+
+eget sharkdp/bat
+eget sharkdp/fd
+eget BurntSushi/ripgrep
+eget jesseduffield/lazygit
+eget jesseduffield/lazydocker
+eget ogham/exa
+eget dbrgn/tealdeer
+eget watchexec/watchexec
+eget bootandy/dust
+
+popd
