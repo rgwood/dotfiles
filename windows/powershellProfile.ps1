ece88e90c11d27cd6809aed4f3ec9e66c4c57edd
Reilly Wood <reilly.wood@icloud.com>
2022-01-06 21:12:11 -0800

    Add build-singlefile command for .NET single file build

--- windows/powershellProfile.ps1
+++ windows/powershellProfile.ps1
@@ -15,6 +15,48 @@ function gs(){git status}
 function gitlog(){git log --oneline --graph}
 function lg(){lazygit}
 
+# .NET stuff
+
+Add-Type -Language CSharp @"
+using System;
+using System.Runtime.InteropServices;
+namespace Profile;
+
+public static class Utils
+{
+	public static string GetRid()
+	{
+		string os;
+
+		if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+			os = "osx";
+		else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+			os = "win";
+		else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+			os = "linux";
+		else
+			throw new Exception("Unsupported OS");
+
+		string arch = RuntimeInformation.OSArchitecture switch
+		{
+			Architecture.X64 => "x64",
+			Architecture.Arm64 => "arm64",
+			Architecture.X86 => "x86",
+			Architecture.Arm => "arm",
+			_ => throw new Exception("Unsupported architecture")
+		};
+
+		return $"{os}-{arch}";
+	}
+}
+"@;
+
+function build-singlefile($rid) 
+{
+    $rid = $rid ?? [Profile.Utils]::GetRid()
+    dotnet publish --configuration Release --runtime $rid --self-contained true -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true -p:DebugType=embedded --output .
+}
+
 # Keybindings
 Set-PSReadLineKeyHandler -key Tab -Function MenuComplete
 
