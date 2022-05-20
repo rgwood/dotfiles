9ce1a8b940a18ef8fb149c0d732a2419ae39bcc8
Reilly Wood <reilly.wood@icloud.com>
2022-05-20 13:26:55 -0700

    comment out unused links

--- windows/setupLinks.ps1
+++ windows/setupLinks.ps1
@@ -1,8 +1,8 @@
 # PS profile: Current user, all hosts
-New-Item -ItemType symboliclink -Path "$HOME\OneDrive\Documents\PowerShell\profile.ps1" -Value "$HOME\dotfiles\windows\powershellProfile.ps1" -Force
+# New-Item -ItemType symboliclink -Path "$HOME\OneDrive\Documents\PowerShell\profile.ps1" -Value "$HOME\dotfiles\windows\powershellProfile.ps1" -Force
 
 # Is there any difference between $HOME and $Env:Userprofile?
-New-Item -ItemType symboliclink -Path "$Env:Userprofile\.npmrc" -Value "$Env:Userprofile\dotfiles\Windows\npmrc"
+# New-Item -ItemType symboliclink -Path "$Env:Userprofile\.npmrc" -Value "$Env:Userprofile\dotfiles\Windows\npmrc"
 
 # Hardlink b/c Windows Terminal settings don't auto-reload with symlinks
 # https://github.com/microsoft/terminal/issues/1812
@@ -10,7 +10,7 @@ New-Item -ItemType symboliclink -Path "$Env:Userprofile\.npmrc" -Value "$Env:Use
 
 New-Item -ItemType symboliclink -Path "$HOME\.config\starship.toml" -Value "$HOME\dotfiles\starship.toml" -Force
 
-New-Item -ItemType symboliclink -Path "$HOME\scripts" -Value "$HOME\dotfiles\scripts\win" -Force
+# New-Item -ItemType symboliclink -Path "$HOME\scripts" -Value "$HOME\dotfiles\scripts\win" -Force
 
 New-Item -ItemType symboliclink -Path "$env:AppData\lazygit\config.yml" -Value "$HOME\dotfiles\Windows\lazygit-config.yml" -Force
 
