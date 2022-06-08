73e8acc4eb4d52b7686f3d41d29f89c7d7e5a35b
Reilly Wood <reilly.wood@icloud.com>
2022-06-07 23:34:26 -0700

    filetime helper

--- templates/windows_utils.rs
+++ templates/windows_utils.rs
@@ -18,3 +18,16 @@ pub fn to_windows_wide_string(string: &str) -> Vec<u16> {
         .chain(std::iter::once(0))
         .collect()
 }
+
+
+pub fn unix_time_from_filetime(ft: &windows::Win32::Foundation::FILETIME) -> i64 {
+    /// January 1, 1970 as Windows file time
+    const EPOCH_AS_FILETIME: u64 = 116444736000000000;
+    const HUNDREDS_OF_NANOSECONDS: u64 = 10000000;
+
+    let time_u64 = ((ft.dwHighDateTime as u64) << 32) | (ft.dwLowDateTime as u64);
+    let rel_to_linux_epoch = time_u64 - EPOCH_AS_FILETIME;
+    let seconds_since_unix_epoch = rel_to_linux_epoch / HUNDREDS_OF_NANOSECONDS;
+
+    seconds_since_unix_epoch as i64
+}
