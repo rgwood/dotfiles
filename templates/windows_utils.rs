f8118819476a5f7eee4f5144a730d28eaf4ff9ac
Reilly Wood <reilly.wood@icloud.com>
2022-06-21 15:44:24 -0700

    lf

--- templates/windows_utils.rs
+++ templates/windows_utils.rs
@@ -30,3 +30,39 @@ pub fn unix_time_from_filetime(ft: &windows::Win32::Foundation::FILETIME) -> i64
 
     seconds_since_unix_epoch as i64
 }
+
+// A port of Superluminal's C++ dark theme check: https://gist.github.com/rovarma/7b85f3db80f40cc144866812cad09919
+//
+// The Windows 10 Anniversary Update (1803) introduced support for dark mode. This is mainly intended for UWP apps, but things like Explorer follow it as well.
+// There is no API to query the value of this setting directly, but it is stored in the registry, so we can simply read it from there.
+// Note that if the regkey is not found in the registry (i.e. in earlier versions of Windows), we default to light theme.
+fn is_dark_theme_active() -> bool {
+    let hkey = HKEY_CURRENT_USER;
+    let lpsubkey = windows_utils::to_windows_wide_string(
+        "Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize",
+    );
+    let lpvalue = windows_utils::to_windows_wide_string("AppsUseLightTheme");
+    let mut pcbdata = size_of::<u32>() as u32;
+    let mut pvdata: u32 = Default::default();
+
+    unsafe {
+        // Read value of the registry key; if it's 0 it means dark theme is enabled.
+        let res = RegGetValueW(
+            hkey,
+            PCWSTR(lpsubkey.as_ptr()),
+            PCWSTR(lpvalue.as_ptr()),
+            RRF_RT_REG_DWORD, // force RegGetValue to return a DWORD
+            std::ptr::null_mut(),
+            &mut pvdata as *mut u32 as *mut c_void,
+            &mut pcbdata,
+        );
+
+        if res == ERROR_SUCCESS {
+            eprintln!("great success");
+            return pvdata == 0;
+        }
+    }
+
+    // Regkey not found, default to light theme.
+    false
+}
