25cd016a6711c13eed90e2d8b4b08636edbc6412
Reilly Wood <reilly.wood@icloud.com>
2022-07-05 16:37:41 -0700

    disable .NET via apt-get temporarily

--- linux/ubuntu21.10.sh
+++ linux/ubuntu21.10.sh
@@ -12,11 +12,11 @@ sudo apt install build-essential llvm libclang-dev libssl-dev pkg-config -y
 
 # Install .NET
 # TODO update this when Microsoft publishes 21.10 version, but this works for now
-wget https://packages.microsoft.com/config/ubuntu/21.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
-rm packages-microsoft-prod.deb
-
-sudo apt-get update; \
-  sudo apt-get install -y apt-transport-https && \
-  sudo apt-get update && \
-  sudo apt-get install -y dotnet-sdk-6.0
+# wget https://packages.microsoft.com/config/ubuntu/21.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+# sudo dpkg -i packages-microsoft-prod.deb
+# rm packages-microsoft-prod.deb
+#
+# sudo apt-get update; \
+#   sudo apt-get install -y apt-transport-https && \
+#   sudo apt-get update && \
+#   sudo apt-get install -y dotnet-sdk-6.0
