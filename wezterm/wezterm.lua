ba4a40b5b376dc07f1d23e9eb1305cfc6209e0c3
Reilly Wood <reilly@urbanlogiq.com>
2023-07-28 11:47:31 -0700

    wezterm m ac

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,9 +6,14 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
--- For example, changing the color scheme:
 config.color_scheme = 'AdventureTime'
-config.default_prog = {'/home/linuxbrew/.linuxbrew/bin/nu'}
+
+-- if on linux use linuxbrew, if on mac use homebrew
+if wezterm.target_triple == "x86_64-unknown-linux-gnu" then
+    config.default_prog = {'/home/linuxbrew/.linuxbrew/bin/nu'}
+elseif string.find(wezterm.target_triple, 'apple%-darwin') then
+    config.default_prog = {'/opt/homebrew/bin/nu'}
+end
 
 -- and finally, return the configuration to wezterm
 return config
