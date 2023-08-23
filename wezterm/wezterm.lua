02707b63cea53c9963eb9f6c2f8dae393b1e6303
Reilly Wood <reilly.wood@icloud.com>
2023-08-22 18:11:12 -0700

    wezterm nicer tab bar

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -21,6 +21,9 @@ elseif string.find(wezterm.target_triple, 'apple%-darwin') then
     config.default_prog = {'/opt/homebrew/bin/nu'}
 end
 
+config.hide_tab_bar_if_only_one_tab = true
+config.show_tab_index_in_tab_bar = false
+
 config.keys = {
   {key="v", mods="CTRL", action=wezterm.action{PasteFrom="Clipboard"}},
   {
