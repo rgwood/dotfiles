5cb424f9234f5efce6b0256f1f403584deda5015
Reilly Wood <reilly.wood@icloud.com>
2024-05-05 20:50:54 -0700

    ctrl-c copy

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,8 +1,9 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
+local act = wezterm.action
 
 -- This table will hold the configuration.
-local config = wezterm.config_builder()
+local cfg = wezterm.config_builder()
 
 local is_linux = function()
 	return wezterm.target_triple:find("linux") ~= nil
@@ -19,25 +20,25 @@ end
 -- This is where you actually apply your config choices
 
 -- config.color_scheme = 'AdventureTime'
-config.color_scheme = 'Solarized Dark Higher Contrast'
+cfg.color_scheme = 'Solarized Dark Higher Contrast'
 -- config.color_scheme = 'Sonokai (Gogh)'
 
-config.font = wezterm.font 'Consolas NF'
-config.font_size = 12.0
+cfg.font = wezterm.font 'Consolas NF'
+cfg.font_size = 11.0
 
 if is_windows() then
   -- config.default_prog = { os.getenv("USERPROFILE") .. '\\bin\\nu' }
   -- winget location
-  config.default_prog = { 'C:\\Program Files\\nu\\bin\\nu.exe' }
+  cfg.default_prog = { 'C:\\Program Files\\nu\\bin\\nu.exe' }
   -- needed to get the OpenSSH Agent working on Windows
   -- https://github.com/wez/wezterm/discussions/3772#discussioncomment-7201688
-  config.ssh_backend = "Ssh2"
+  cfg.ssh_backend = "Ssh2"
 else
-  config.default_prog = { os.getenv("HOME") .. '/bin/nu' }
+  cfg.default_prog = { os.getenv("HOME") .. '/bin/nu' }
 end
 
-config.window_close_confirmation = 'NeverPrompt'
-config.skip_close_confirmation_for_processes_named = {
+cfg.window_close_confirmation = 'NeverPrompt'
+cfg.skip_close_confirmation_for_processes_named = {
   'bash',
   'sh',
   'zsh',
@@ -53,12 +54,12 @@ config.skip_close_confirmation_for_processes_named = {
 -- config.enable_scroll_bar = true
 -- config.hide_tab_bar_if_only_one_tab = true
 
-config.show_tab_index_in_tab_bar = false
+cfg.show_tab_index_in_tab_bar = false
 
-config.window_decorations = 'INTEGRATED_BUTTONS|RESIZE'
-config.win32_system_backdrop = 'Acrylic'
+cfg.window_decorations = 'INTEGRATED_BUTTONS|RESIZE'
+cfg.win32_system_backdrop = 'Acrylic'
 
-config.keys = {
+cfg.keys = {
   {key="v", mods="CTRL", action=wezterm.action{PasteFrom="Clipboard"}},
   {
     key = "UpArrow",
@@ -73,6 +74,28 @@ config.keys = {
       ScrollToPrompt = 1
     }
   }
+}
+
+local copy_mode = wezterm.gui.default_key_tables().copy_mode
+-- remove existing ctrl+c keybinding
+for i, v in ipairs(copy_mode) do
+    if v.key == 'c' and v.mods == 'CTRL' then
+        table.remove(copy_mode, i)
+        break
+    end
+end
+table.insert(copy_mode, {
+    key = 'c',
+    mods = 'CTRL',
+    action = act.Multiple {{
+        CopyTo = 'ClipboardAndPrimarySelection'
+    }, {
+        CopyMode = 'Close'
+    }}
+})
+
+cfg.key_tables = {
+    copy_mode = copy_mode
 }
 
 function formatDomain(domain)
@@ -96,5 +119,5 @@ return title
 end)
 
 -- and finally, return the configuration to wezterm
-return config
+return cfg
 
